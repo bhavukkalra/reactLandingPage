e1b973267f75b1d0a7316c167beb38e63deb2a5c
bhavukkalra <bhavukkalra1786@gmail.com>
2024-04-16 00:46:08 +0530

    Add comments for understnading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+/*
+Different between using class and extending component from react and 
+using functions to render components?
+
+Note - <img /> is a self closing tag
+*/
+
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom"></img>
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+/*
+<Header/> or <Header></Header>
+Both acceptable
+*/
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header>This is text inside header</Header>
       <main>
         <h2>Time to get started!</h2>
       </main>
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
+/*
+he CSS rules defined in index.css are applied globally within the scope of your React application, 
+styling all elements rendered within <App /> and its child components.
+*/
+
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
