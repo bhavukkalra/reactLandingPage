148bbfcfd95e716d345d0a5dda6f5ebf0d035f2c
bhavukkalra <bhavukkalra1786@gmail.com>
2024-04-16 01:46:45 +0530

    add images in React Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 /*
+Q/A importing an Image file to react project, Handled by the build process
+*/
+
+import reactImg from './assets/react-core-concepts.png'
+
+/*
 Different between using class and extending component from react and 
 using functions to render components?
 
@@ -15,6 +21,22 @@ function genRandomInt(max){
 {} => Cannot contain for, if else, while loops. Only expression that produce a result
 */
 
+
+/*
+Q/A
+
+Image files when imported like this - 
+src="src/assets/react-core-concepts.png"
+
+might be ignored by the bundling process i.e production build.
+(Even tho they look fine in dev)
+
+So the best way to use them is to (import them)
+
+import reactAtom from './path/to/atom/image'
+
+*/
+
 function Header() {
   const description = reactDescriptions[genRandomInt(2)];
   console.log(description)
@@ -22,7 +44,7 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom"></img>
+      <img src={reactImg} alt="Stylized atom"></img>
       <h1>React Essentials</h1>
       <p>
         {description}  React concepts you will need for almost any app you are
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,27 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 /*
-he CSS rules defined in index.css are applied globally within the scope of your React application, 
+the CSS rules defined in index.css are applied globally within the scope of your React application, 
 styling all elements rendered within <App /> and its child components.
 */
+
+/*
+Normally these types of imports don't work in a sample js file
+but in jsx they do work
+
+Q/A
+*/
+
+
 
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
+/*
+Renders the root component App and any other nested components it might have
+i.e start of rendering of react component tree
+*/
+
+
+
 ReactDOM.createRoot(entryPoint).render(<App />);
