a8cb4e2b9883ec78e03d4464a2c8ac9d0c9dcd2a
bhavukkalra <bhavukkalra1786@gmail.com>
2024-04-16 01:34:26 +0530

    Render the first keyword dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,27 @@ using functions to render components?
 Note - <img /> is a self closing tag
 */
 
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+
+function genRandomInt(max){
+  return Math.floor(Math.random() * (max + 1));
+}
+
+/*
+{} => Cannot contain for, if else, while loops. Only expression that produce a result
+*/
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+  console.log(description)
+
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom"></img>
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description}  React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
