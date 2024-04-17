4393a6e1e2050a80700e9f47f996298a4d04f216
bhavukkalra <bhavukkalra1786@gmail.com>
2024-04-17 14:22:15 +0530

    format changes

--- src/App.jsx
+++ src/App.jsx
@@ -100,14 +100,11 @@ function App() {
           <h2>Core Concepts</h2>
 
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}
-            />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
 
-            <CoreConcept {...CORE_CONCEPTS[1]}
-            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
 
-<CoreConcept {...CORE_CONCEPTS[2]}
-            />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
 
             <CoreConcept
               title={CORE_CONCEPTS[3].title}
